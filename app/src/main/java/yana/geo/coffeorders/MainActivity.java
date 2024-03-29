1cc1bb1cd74d61f32cdeaaedc515d00224f77d4c
Yana Geo <94054850+YanaGG@users.noreply.github.com>
2024-03-29 23:22:58 +0200

    29-03-2024-23-22

--- app/src/main/java/yana/geo/coffeorders/MainActivity.java
+++ app/src/main/java/yana/geo/coffeorders/MainActivity.java
@@ -2,13 +2,38 @@ package yana.geo.coffeorders;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextClientName;
+    private EditText editTextClientPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextClientName = findViewById(R.id.editTextClientName);
+        editTextClientPassword = findViewById(R.id.editTextClientPassword);
+    }
+
+    public void CreateOrderOnClick(View view) {
+        String name = editTextClientName.getText().toString().trim();
+        String password = editTextClientPassword.getText().toString().trim();
+        if (!name.isEmpty() && !password.isEmpty()){
+            Intent intent = new Intent(this, CreateOrderActivity.class);
+            intent.putExtra("name", name);
+            intent.putExtra("password", password);
+            startActivity(intent);
+        }
+        else {
+            Toast.makeText(this,R.string.toast_text_empty_fields, Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
